c339dc920555a6e6721b5d8df195813a7dfef630
Abhinav Singh <abhi17may2004@gmail.com>
2025-07-30 22:49:39 +0530

    url change

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL:'https://proj-backend-un8b.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
